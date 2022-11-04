93544e4a8f04ae9ec5edaeae3ac78517508e549b
Angel Diaz <a_angel.diaz.aviles@iespablopicasso.es>
2022-11-04 10:35:01 +0100

    Piramide fotos

--- primeritaapp/lib/main.dart
+++ primeritaapp/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    	title: 'filas y columnas anidadas',
+    title: 'filas y columnas anidadas',
     home: Scaffold(
       appBar: AppBar(
         title: Center(
@@ -18,20 +18,30 @@ void main() {
               Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Image.asset('assets/images/call.jpg', 
-                  width: 200,
-                  height: 200),
-                   
-                  Text('Llamar por teléfono'),
+                  Image.asset('assets/images/movil.jpg',
+                      width: 200, height: 200),
+                  Text('\nMóvil'),
+                ],
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset('assets/images/call.jpg',
+                      width: 200, height: 200),
+                  Text('\nLlamar por teléfono'),
                 ],
               ),
               Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Image.asset('assets/images/mensaje.jpg', 
-                  width: 200,
-                  height: 200),
-                  Text('Enviar mensaje'),
+                  Image.asset('assets/images/mensaje.jpg',
+                      width: 200, height: 200),
+                  Text('\nEnviar mensaje'),
                 ],
               ),
             ],
@@ -43,26 +53,31 @@ void main() {
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   Image.asset('assets/images/instagram.jpg',
-                  width: 200,
-                  height: 200),
-                  Text('Instagram'),
+                      width: 200, height: 200),
+                  Text('\nInstagram'),
                 ],
               ),
               Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Image.asset('assets/images/whatsapp.jpg', 
-                  width: 200,
-                  height: 200),
-                  Text('Whatsapp'),
+                  Image.asset('assets/images/whatsapp.jpg',
+                      width: 200, height: 200),
+                  Text('\nWhatsapp'),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset('assets/images/tiktok.jpg',
+                      width: 200, height: 200),
+                  Text('\nTiktok'),
                 ],
               ),
             ],
           ),
+
         ],
       ),
     ),
   ));
-
 }
-
