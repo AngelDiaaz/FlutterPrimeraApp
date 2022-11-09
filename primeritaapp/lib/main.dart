d609c3204735b738d22c03a3dcca2a201a0a1811
adiaz <a_angel.diaz.aviles@iespablopicasso.es>
2022-11-09 19:14:41 +0100

    Fuente cambiada

--- primeritaapp/lib/main.dart
+++ primeritaapp/lib/main.dart
@@ -1,83 +1,50 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    title: 'filas y columnas anidadas',
-    home: Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Text('Ejemplo de filas y columnas anidadas'),
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Custom Fonts',
+      theme: ThemeData(fontFamily: 'PoorStory'),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Custom Fonts')),
+      body: Center(
+        child: Text("En un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo que "
+            "vivía un hidalgo de los de lanza en astillero, adarga antigua, rocín flaco y galgo corredor. "
+            "Una olla de algo más vaca que carnero, salpicón las más noches, duelos y quebrantos los sábados, "
+            "lantejas los viernes, algún palomino de añadidura los domingos, consumían las tres partes de su hacienda."
+            " El resto della concluían sayo de velarte, calzas de velludo para las fiestas, con sus pantuflos de lo "
+            "mesmo, y los días de entresemana se honraba con su vellorí de lo más fino. Tenía en su casa una ama que "
+            "pasaba de los cuarenta y una sobrina que no llegaba a los veinte, y un mozo de campo y plaza que así "
+            "ensillaba el rocín como tomaba la podadera. Frisaba la edad de nuestro hidalgo con los cincuenta años. "
+            "Era de complexión recia, seco de carnes, enjuto de rostro, gran madrugador y amigo de la caza. Quieren "
+            "decir que tenía el sobrenombre de «Quijada», o «Quesada», que en esto hay alguna diferencia en los autores "
+            "que deste caso escriben, aunque por conjeturas verisímiles se deja entender que se llamaba «Quijana». Pero "
+            "esto importa poco a nuestro cuento: basta que en la narración dél no se salga un punto de la verdad.",
+
+          maxLines: 10,
+          overflow: TextOverflow.ellipsis,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontFamily: 'PoorStory',
+            color: Colors.red,
+            fontSize: 30,
+            fontWeight: FontWeight.w400,
+            fontStyle: FontStyle.italic,
+            decoration: TextDecoration.underline,
+          ),
         ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Image.asset('assets/images/movil.jpg',
-                      width: 200, height: 200),
-                  Text('\nMóvil'),
-                ],
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Image.asset('assets/images/call.jpg',
-                      width: 200, height: 200),
-                  Text('\nLlamar por teléfono'),
-                ],
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Image.asset('assets/images/mensaje.jpg',
-                      width: 200, height: 200),
-                  Text('\nEnviar mensaje'),
-                ],
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Image.asset('assets/images/instagram.jpg',
-                      width: 200, height: 200),
-                  Text('\nInstagram'),
-                ],
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Image.asset('assets/images/whatsapp.jpg',
-                      width: 200, height: 200),
-                  Text('\nWhatsapp'),
-                ],
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Image.asset('assets/images/tiktok.jpg',
-                      width: 200, height: 200),
-                  Text('\nTiktok'),
-                ],
-              ),
-            ],
-          ),
-
-        ],
-      ),
-    ),
-  ));
+    );
+  }
 }
