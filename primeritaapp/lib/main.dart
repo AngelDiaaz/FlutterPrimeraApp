03a24f8898b1f790b9a26e7bbbabee1821c75db0
Angel Diaz <a_angel.diaz.aviles@iespablopicasso.es>
2022-11-11 12:29:38 +0100

    Ejercicio4 hecho

--- primeritaapp/lib/main.dart
+++ primeritaapp/lib/main.dart
@@ -2,49 +2,46 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Custom Fonts',
-      theme: ThemeData(fontFamily: 'PoorStory'),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Custom Fonts')),
-      body: Center(
-        child: Text("En un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo que "
-            "vivía un hidalgo de los de lanza en astillero, adarga antigua, rocín flaco y galgo corredor. "
-            "Una olla de algo más vaca que carnero, salpicón las más noches, duelos y quebrantos los sábados, "
-            "lantejas los viernes, algún palomino de añadidura los domingos, consumían las tres partes de su hacienda."
-            " El resto della concluían sayo de velarte, calzas de velludo para las fiestas, con sus pantuflos de lo "
-            "mesmo, y los días de entresemana se honraba con su vellorí de lo más fino. Tenía en su casa una ama que "
-            "pasaba de los cuarenta y una sobrina que no llegaba a los veinte, y un mozo de campo y plaza que así "
-            "ensillaba el rocín como tomaba la podadera. Frisaba la edad de nuestro hidalgo con los cincuenta años. "
-            "Era de complexión recia, seco de carnes, enjuto de rostro, gran madrugador y amigo de la caza. Quieren "
-            "decir que tenía el sobrenombre de «Quijada», o «Quesada», que en esto hay alguna diferencia en los autores "
-            "que deste caso escriben, aunque por conjeturas verisímiles se deja entender que se llamaba «Quijana». Pero "
-            "esto importa poco a nuestro cuento: basta que en la narración dél no se salga un punto de la verdad.",
-          maxLines: 10,
-          overflow: TextOverflow.ellipsis,
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            fontFamily: 'PoorStory',
-            color: Colors.red,
-            fontSize: 30,
-            fontWeight: FontWeight.w400,
-            fontStyle: FontStyle.italic,
-            decoration: TextDecoration.underline,
-          ),
-        ),
-      ),
-    );
+        title: 'Ejemplo contenedores',
+        home: Scaffold(
+            appBar: AppBar(
+              title: Center(
+                child: Text('Ejemplo de Container. Ejercicio 4'),
+              ),
+            ),
+            body: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                      margin: EdgeInsets.all(50),
+                      width: 300,
+                      height: 300,
+                      alignment: Alignment.center,
+                      color: Colors.green,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset('assets/images/movil.jpg'),
+                          Text('\nEjemplo de un container'),
+                    ],
+                  )),
+                  Container(
+                      margin: EdgeInsets.all(50),
+                      width: 300,
+                      height: 300,
+                      alignment: Alignment.center,
+                      color: Colors.red,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset('assets/images/movil.jpg'),
+                          Text('\nEjemplo de un container'),
+                        ],
+                      ))
+                ])));
   }
 }
