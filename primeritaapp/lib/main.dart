1868438ad46fa840ee839d3a7381e859443453a2
adiaz <a_angel.diaz.aviles@iespablopicasso.es>
2022-12-16 18:45:27 +0100

    Ejercicio 10 hecho

--- primeritaapp/lib/main.dart
+++ primeritaapp/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'Pantalla.dart';
 
 void main() => runApp(const MyApp());
 
@@ -9,44 +8,96 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Sample App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const AnimateApp(),
+    );
+  }
+}
+
+class AnimateApp extends StatefulWidget {
+  const AnimateApp({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _AnimateAppState();
+  }
+}
+
+class _AnimateAppState extends State<AnimateApp>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> animation;
+  late CurvedAnimation curve;
+  late double _widthImage;
+
+  @override
+  void initState() {
+    super.initState();
+    // Create AnimationController object
+    controller = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 2000));
+    curve = CurvedAnimation(parent: controller, curve: Curves.easeInQuint);
+    // Create Animation object through Tween object
+    animation = Tween(begin: 50.0, end: 200.0).animate(controller)
+      ..addListener(() {
+        // Note: This sentence cannot be omitted, otherwise the widget will not be redrawn and the animation effect will not be seen
+        setState(() {});
+      });
+    _widthImage = 150;
+    // Perform animation
+    controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'AnimateApp',
         debugShowCheckedModeBanner: false,
-        title: 'Probando drawer',
+        theme: ThemeData(primaryColor: Colors.blue),
         home: Scaffold(
             appBar: AppBar(
-              title: const Center(
-                child: Text('Ejercicio 7'),
-              ),
+              title: const Text('AnimateApp'),
             ),
-            drawer: const MenuLateral(),
-            body: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                      margin: const EdgeInsets.all(50),
-                      width: 300,
-                      height: 300,
-                      alignment: Alignment.center,
-                      color: Colors.green,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image.asset('assets/images/movil.jpg'),
-                          const Text('\nEjemplo de un container 1'),
-                        ],
-                      )),
-                  Container(
-                      margin: const EdgeInsets.all(50),
-                      width: 300,
-                      height: 300,
-                      alignment: Alignment.center,
-                      color: Colors.red,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image.asset('assets/images/call.jpg'),
-                          const Text('\nEjemplo de un container 2'),
-                        ],
-                      ))
-                ])));
+            body: Center(
+              child: GestureDetector(
+                child: RotationTransition(
+                    turns: curve,
+                    child: Image(
+                      image: const AssetImage("assets/images/f1.jpg"),
+                      width: _widthImage,
+                    )),
+                onTap: () {
+                  if (controller.isCompleted) {
+                    controller.reverse();
+                    _widthImage = 300;
+                  } else {
+                    controller.forward();
+                    _widthImage = 150;
+                  }
+                  curve = CurvedAnimation(
+                      parent: controller, curve: Curves.elasticIn);
+                },
+                onDoubleTap: () {
+                  if (controller.isCompleted) {
+                    controller.reverse();
+                  } else {
+                    controller.forward();
+                  }
+                  curve = CurvedAnimation(
+                      parent: controller, curve: Curves.bounceInOut);
+                },
+              ),
+            )));
+  }
+
+  @override
+  void dispose() {
+    // Resource release
+    controller.dispose();
+    super.dispose();
   }
 }
