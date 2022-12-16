6d65a67d3b38a220f0b0b821787cc2283b3c938a
Ángel Díaz Avilés <93326288+AngelDiaaz@users.noreply.github.com>
2022-12-16 12:07:40 +0100

    Ejercicio 9 hecho

--- primeritaapp/lib/main.dart
+++ primeritaapp/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'Pantalla.dart';
 
 void main() => runApp(const MyApp());
 
@@ -14,39 +13,107 @@ class MyApp extends StatelessWidget {
         home: Scaffold(
             appBar: AppBar(
               title: const Center(
-                child: Text('Ejercicio 7'),
+                child: Text('Ejercicio 9'),
               ),
             ),
-            drawer: const MenuLateral(),
-            body: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                      margin: const EdgeInsets.all(50),
-                      width: 300,
-                      height: 300,
-                      alignment: Alignment.center,
-                      color: Colors.green,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image.asset('assets/images/movil.jpg'),
-                          const Text('\nEjemplo de un container 1'),
-                        ],
-                      )),
-                  Container(
-                      margin: const EdgeInsets.all(50),
-                      width: 300,
-                      height: 300,
-                      alignment: Alignment.center,
-                      color: Colors.red,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image.asset('assets/images/call.jpg'),
-                          const Text('\nEjemplo de un container 2'),
-                        ],
-                      ))
-                ])));
+            body: GridView.count(
+              primary: false,
+              padding: const EdgeInsets.all(20),
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              crossAxisCount: 2,
+              children: <Widget>[
+                Container(
+                    padding: const EdgeInsets.all(8),
+                    color: Colors.teal[100],
+                    child: Align(
+                        child: OutlinedButton(
+                      child: const Text('Outlined Button'),
+                      onPressed: () {},
+                    ))),
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  color: Colors.teal[100],
+                  child: Align(
+                    child:
+                    TextButton(
+                      child: const Text('Text Button'),
+                      onPressed: () {},
+                    ),
+                  )
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  color: Colors.teal[100],
+                  child: Align(
+                      child:
+                      IconButton(
+                        tooltip: 'Información',
+                        icon: const Icon(Icons.person),
+                        iconSize: 30,
+                        color: Colors.red,
+                        onPressed: () {},
+                      )
+                  )
+                  ,
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  color: Colors.teal[100],
+                  child: Align(
+                    child: FloatingActionButton(
+                      tooltip: 'Resta',
+                      onPressed: () {},
+                      child: const Icon(Icons.remove),
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  color: Colors.teal[100],
+                  child: Align(
+                      child:
+                      PopupMenuButton<String>(
+                        itemBuilder: (context) {
+                          return <PopupMenuEntry<String>>[
+                            const PopupMenuItem<String>(
+                              value: 'uno',
+                              child: Text('uno'),
+                            ),
+                            const PopupMenuItem<String>(
+                              value: 'dos',
+                              child: Text('dos'),
+                            ),
+                            const PopupMenuItem<String>(
+                              value: 'tres',
+                              child: Text('tres'),
+                            ),
+                            const PopupMenuItem<String>(
+                              value: 'cuatro',
+                              child: Text('cuatro'),
+                            ),
+                            const PopupMenuItem<String>(
+                              value: 'cinco',
+                              child: Text('cinco'),
+                            ),
+                          ];
+                        },
+                      )
+                  )
+                  ,
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  color: Colors.teal[100],
+                  child: Align(
+                    child: ElevatedButton.icon(
+                      label: const Text('Elevated Button'),
+                      icon: const Icon(Icons.android_outlined),
+                      onPressed: () {},
+                    ),
+                  ),
+                ),
+              ],
+            )));
   }
 }
